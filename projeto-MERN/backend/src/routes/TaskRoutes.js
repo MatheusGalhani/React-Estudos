6e21c3acac9d5ed7f58a7413e3f6c208132ac6e4
MatheusGalhani <matheusgalhani@hotmail.com>
2021-01-02 00:45:22 -0300

    delete method

--- projeto-MERN/backend/src/routes/TaskRoutes.js
+++ projeto-MERN/backend/src/routes/TaskRoutes.js
@@ -5,8 +5,13 @@ const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation');
 const MacAddressValidation = require('../middlewares/MacAddressValidation');
 
+/*********** ROTAS ***********/
 router.post('/', TaskValidation, TaskController.create);
+
 router.put('/:id', TaskController.update);
+router.get('/:id', TaskController.show);
+router.delete('/:id', TaskController.delete);
+
 router.get('/filter/all', MacAddressValidation, TaskController.all);
 
 module.exports = router;
