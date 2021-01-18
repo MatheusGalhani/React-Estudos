50d174c8af018cc638edcc2120c1a2a8884e91ca
Matheus Galhani <matheusgalhani@hotmail.com>
2021-01-18 09:16:41 -0300

    Create folder src

---
+++ projeto-MERN/frontend/mobile/src/views/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles';
+
+function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>Bem vindo a tela Home</Text>
+        </View>
+    )
+}
+
+
+export default Home;
